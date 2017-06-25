c21703e09a901800b3750265d7273dd70a13ac03
kazuooooo <matsumotokazuya7@gmail.com>
2017-06-25 13:59:50 +0900

    remove

--- app/assets/javascripts/script.js
+++
@@ -1,9 +0,0 @@
-$(".icon_wrap .icon").click(function(){
-  if($(this).hasClass("non_active")){ // クリックされた要素がclickedクラスだったら
-    $(this).removeClass("non_active");
-    $(this).addClass("active");
-  }else{
-    $(this).removeClass("active");
-    $(this).addClass("non_active");
-  }
-});
