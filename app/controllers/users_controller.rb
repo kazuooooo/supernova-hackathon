ea819e40a5d7d1e734623c9dabf0ce7f339b6e19
kazuooooo <matsumotokazuya7@gmail.com>
2017-06-25 11:03:10 +0900

    add base routes and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,28 @@
 class UsersController < ApplicationController
+
+  def register
+
+  end
+
   def profile
     @phenotypes = User.find(params[:id]).get_phenotypes
   end
 
+  def survey
+
+  end
+
   def match
+
+  end
+
+  def result
     users           = params[:users]
     #  user_phenotypes = users.map{|user| user.get_phenotypes}
     user_phenotypes = User.all.map { |user| user.get_phenotypes }
     @calc_result    = Calculator.instance.sum(user_phenotypes)
     keywords        = Detector.instance.detect(@calc_result)
-    @shops           = ShopClient.instance.search_keywords(keywords.map{|elem| elem.keys}.flatten)
+    @shops          = ShopClient.instance.search_keywords(keywords.map { |elem| elem.keys }.flatten)
     @keys           = @shops.first.to_h.keys
   end
 end
